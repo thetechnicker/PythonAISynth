4f48612ba644fe58dc581289497af6f517809a1a
Thetecknicker <https.LocalHorst@gmx.de>
2024-06-06 12:46:22 +0200

    nothing
    plaid arround with some variables, added some kind of logging

--- scr/fourier_neural_network.py
+++ scr/fourier_neural_network.py
@@ -4,18 +4,18 @@ os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"  # Set PlaidML as the back
 import random
 import numpy as np
 from keras.models import Sequential, load_model  # Import from keras instead of tensorflow.keras
-from keras.layers import Dense, Input  # Import from keras instead of tensorflow.keras
+from keras.layers import Dense  # Import from keras instead of tensorflow.keras
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 
 class FourierNN():
-    RANGE = 10**3
-    EPOCHS = 50
-    TRAINING_SPEED = 10
-    SIGNED_RANDOMNES = 0.000000001
+    RANGE = 10**7
+    ITTERRATIONS = 10
+    EPOCHS = 10
+    SIGNED_RANDOMNES = 0.00000001
     DEFAULT_FORIER_DEGREE = 100
     FORIER_DEGREE_DIVIDER = 100
-    FORIER_DEGREE_OFFSET = 20
+    FORIER_DEGREE_OFFSET = 100
 
     def __init__(self, data):
         self.data = data
@@ -23,6 +23,7 @@ class FourierNN():
 
     @staticmethod
     def fourier_basis(x, n=DEFAULT_FORIER_DEGREE):
+        print("fourier_basis", x)
         basis = [np.sin(i * x) for i in range(1, n+1)]
         basis += [np.cos(i * x) for i in range(1, n+1)]
         return np.array(basis)
@@ -33,25 +34,42 @@ class FourierNN():
         return np.array(basis)
 
     def generate_data(self):
+        print('generate_data')
         fourier_degree = (len(self.data) // self.FORIER_DEGREE_DIVIDER) + self.FORIER_DEGREE_OFFSET
+        print('fourier_degree', fourier_degree)
         actualData_x, actualData_y = zip(*self.data)
+        actualData_x = np.array(actualData_x)
+        actualData_y = np.array(actualData_y)
+        print(len(self.data), self.RANGE/len(self.data), self.RANGE)
         if len(self.data) < self.RANGE:
-            if len(self.data) == self.RANGE / 2:
-                self.data.extend(self.data)
-            else:
-                multi = self.RANGE // len(self.data)
-                rest = self.RANGE % len(self.data)
-                self.data.extend([(dat[0] + random.uniform(-self.SIGNED_RANDOMNES, self.SIGNED_RANDOMNES),
-                                   dat[1] + random.uniform(-self.SIGNED_RANDOMNES, self.SIGNED_RANDOMNES)) for dat in self.data * multi])
-                for i in range(rest):
-                    rand_x = random.uniform(-self.SIGNED_RANDOMNES, self.SIGNED_RANDOMNES)
-                    rand_y = random.uniform(-self.SIGNED_RANDOMNES, self.SIGNED_RANDOMNES)
-                    self.data.append((self.data[i][0] + rand_x, self.data[i][1] + rand_y))
+            print("extending data")
+            ittr=0
+            while len(self.data) < self.RANGE:
+                print("itterration:", ittr, 'len', len(self.data), self.RANGE/len(self.data), self.RANGE)
+                for i, data in enumerate(self.data):
+                    print("element:", i, 'len', len(self.data), self.RANGE/len(self.data), self.RANGE)
+                    if len(self.data) == self.RANGE:
+                        break
+                    new_data=(self.data[i-1]+data)/2
+                    self.data= np.insert(self.data, i+1, new_data)
+                ittr-=-1
+        # if len(self.data) < self.RANGE:
+        #     if len(self.data) == self.RANGE / 2:
+        #         self.data.extend(self.data)
+        #     else:
+        #         multi = self.RANGE // len(self.data)
+        #         rest = self.RANGE % len(self.data)
+        #         self.data.extend([(dat[0] + random.uniform(-self.SIGNED_RANDOMNES, self.SIGNED_RANDOMNES),
+        #                            dat[1] + random.uniform(-self.SIGNED_RANDOMNES, self.SIGNED_RANDOMNES)) for dat in self.data * multi])
+        #         for i in range(rest):
+        #             rand_x = random.uniform(-self.SIGNED_RANDOMNES, self.SIGNED_RANDOMNES)
+        #             rand_y = random.uniform(-self.SIGNED_RANDOMNES, self.SIGNED_RANDOMNES)
+        #             self.data.append((self.data[i][0] + rand_x, self.data[i][1] + rand_y))
         x_train, y_train = zip(*self.data)
         x_train = np.array(x_train)
         y_train = np.array(y_train)
         x_train_transformed = np.array([self.fourier_basis(x, fourier_degree) for x in x_train])
-        return x_train, x_train_transformed, y_train, actualData_x, actualData_y, fourier_degree
+        return x_train_transformed, y_train, actualData_x, actualData_y, fourier_degree
 
     def create_model(self, input_shape):
         self.model = Sequential([
@@ -62,8 +80,10 @@ class FourierNN():
         self.model.compile(optimizer='adam', loss='mean_squared_error')
 
     def train_and_plot(self):
-        x_train, x_train_transformed, y_train, actualData_x, actualData_y, fourier_degree = self.generate_data()
+        print('train_and_plot')
+        x_train_transformed, y_train, actualData_x, actualData_y, fourier_degree = self.generate_data()
         self.create_model((x_train_transformed.shape[1],))
+        print('uff')
 
         plt.ion()
         fig, ax = plt.subplots()
@@ -78,9 +98,9 @@ class FourierNN():
         plt.draw()
         plt.pause(1)
 
-        for epoch in range(self.EPOCHS):
-            print("epoch ", epoch + 1, "/", self.EPOCHS, sep="")
-            self.model.fit(x_train_transformed, y_train, epochs=self.TRAINING_SPEED, batch_size=32, verbose=0)
+        for epoch in range(self.ITTERRATIONS):
+            print("Itterration ", epoch + 1, "/", self.ITTERRATIONS, sep="")
+            self.model.fit(x_train_transformed, y_train, epochs=self.EPOCHS, batch_size=32, verbose=1)
 
             ax.clear()
             ax.plot(_x, np.sin(_x), color='black', label='Base Frequency')
@@ -117,7 +137,7 @@ if __name__ == '__main__':
     def relu(x):
         return np.maximum(0, x)
     # Test the class with custom data points
-    data = [(x, np.sin(x * relu(x))) for x in np.linspace(np.pi, -np.pi, 100)]
+    data = np.array([(x, np.sin(x * relu(x))) for x in np.linspace(np.pi, -np.pi, 1000)])
     fourier_nn = FourierNN(data)
     fourier_nn.train_and_plot()
     # fourier_nn.convert_to_audio()
