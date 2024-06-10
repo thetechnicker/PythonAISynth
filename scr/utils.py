b77014c77bee6ffaf844fbf6f707f9a5f16ea349
thetechnicker <https.LocalHorst@gmx.de>
2024-06-10 08:12:09 +0200

    Update utils.py
    added function `pair_iterator`

--- scr/utils.py
+++ scr/utils.py
@@ -3,3 +3,8 @@ import numpy as np
 
 def midi_to_freq(midi_note):
     return 440.0 * np.power(2.0, (midi_note - 69) / 12.0)
+
+
+def pair_iterator(lst):
+    for i in range(len(lst) - 1):
+        yield lst[i], lst[i+1]
