03fc5e71a15b829846315983b2cb547e8d7795cf
thetechnicker <https.LocalHorst@gmx.de>
2024-07-03 19:20:57 +0200

    stuff

--- cpp_synth/src/main.cpp
+++ cpp_synth/src/main.cpp
@@ -73,20 +73,26 @@ int main()
     {
       printf("Operation: %s\n", TF_OperationName(oper));
     }
-    TF_Operation *input_op = TF_GraphOperationByName(graph, "serving_default_inputs");
-    TF_Output input = {input_op, 0};
-    int num_dims = TF_GraphGetTensorNumDims(graph, input, status);
-    int64_t *dims = (int64_t *)malloc(num_dims * sizeof(int));
-    TF_GraphGetTensorShape(graph, input, dims, num_dims, status);
-    for (int i = 0; i < num_dims; i++)
+
     {
-      printf("input no %d: %ld\n", i, dims[i]);
-      if (i == 1)
+      TF_Operation *input_op_test = TF_GraphOperationByName(graph, "serving_default_inputs");
+      TF_Output input = {input_op_test, 0};
+      int num_dims = TF_GraphGetTensorNumDims(graph, input, status);
+      int64_t *dims = (int64_t *)malloc(num_dims * sizeof(int));
+      TF_GraphGetTensorShape(graph, input, dims, num_dims, status);
+      for (int i = 0; i < num_dims; i++)
       {
-        fourier_degree = (int)(dims[i] / 2);
+        printf("input no %d: %ld\n", i, dims[i]);
+        if (i == 1)
+        {
+          fourier_degree = (int)(dims[i] / 2);
+        }
       }
     }
+
     fourier_test();
+
+    printf("\n");
 
     int n = fourier_degree;
     double x[] = {0.1, 0.2, 0.3, 0.4, 0.5}; // replace with your array
@@ -94,18 +100,65 @@ int main()
     double **basis = (double **)malloc(x_length * sizeof(double *));
 
     fourier_basis(x, x_length, n, basis);
+    int y_length = 2 * n;
 
+    float *flat_array = (float *)malloc(x_length * y_length * sizeof(float));
+    for (int i = 0; i < x_length; ++i)
+    {
+      for (int j = 0; j < y_length; ++j)
+      {
+        flat_array[i * y_length + j] = (float)basis[i][j];
+      }
+    }
     for (int i = 0; i < x_length; ++i)
     {
       free(basis[i]);
     }
     free(basis);
+
+    int64_t dims[] = {x_length, y_length};
+    size_t num_bytes = x_length * y_length * sizeof(float);
+    TF_Tensor *tensor = TF_AllocateTensor(TF_FLOAT, dims, 2, num_bytes);
+    memcpy(TF_TensorData(tensor), flat_array, num_bytes);
+
+    TF_Output input_op = {TF_GraphOperationByName(graph, "serving_default_inputs"), 0};
+    TF_Output output_op = {TF_GraphOperationByName(graph, "StatefulPartitionedCall"), 0};
+
+    TF_Tensor *output_tensor = NULL;
+
+    TF_SessionRun(session, NULL, &input_op, &tensor, 1, &output_op, &output_tensor, 1, NULL, 0, NULL, status);
+
+
+    printf("\nout_tensor %d\n", output_tensor);
+    if (TF_GetCode(status) != TF_OK)
+    {
+      fprintf(stderr, "Error running session: %s\n", TF_Message(status));
+      goto exit_label;
+    }
+    void *output_data = TF_TensorData(output_tensor);
+    // if (output_data!= NULL)
+    // {
+    //   printf("YEAH\n");
+    // }
+    // else
+    // {
+    //   printf("NOOO\n");
+    // }
+    // int num_elements = TF_TensorElementCount(output_tensor);
+
+    // for (int i = 0; i < num_elements; ++i)
+    // {
+    //   printf("%f\n", output_data[i]);
+    // }
+
+    free(flat_array);
   }
   else
   {
     printf("%s", TF_Message(status));
   }
 
+exit_label:
   // Clean up.
   TF_DeleteGraph(graph);
   TF_DeleteSession(session, status);
