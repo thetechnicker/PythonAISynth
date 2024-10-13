bcdf92a584ba360629f44f046984e350e6a065d9
thetechnicker <https.LocalHorst@gmx.de>
2024-10-13 14:32:59 +0200

    added ADSR Envelope

--- src/music.py
+++ src/music.py
@@ -1,18 +1,10 @@
-import copy
-import threading
-import time
-from typing import Callable
 import mido
-import pygame
 import scipy
 import torch
 from src.fourier_neural_network import FourierNN
 from src import utils
-from pygame import mixer, midi, sndarray
 import atexit
-import multiprocessing
 from multiprocessing import Process, Queue, current_process
-import os
 import sys
 from tkinter import filedialog
 import numpy as np
@@ -40,7 +32,7 @@ def musik_from_file(fourier_nn: FourierNN):
                 print(note_a)
                 duration = note_a.end - note_a.start
                 synthesized_note = fourier_nn.synthesize(
-                    midi_note=note_a.pitch-12, duration=duration, sample_rate=44100)
+                        midi_note=note_a.pitch-12, duration=duration, sample_rate=44100)
                 print(synthesized_note.shape)
                 note_list.append(synthesized_note)
 
@@ -72,206 +64,6 @@ def rescale_audio(audio):
         return audio
     return audio / max_val
 
-
-class Synth():
-
-    MIDI_NOTE_OF_FREQ_ONE = midi.frequency_to_midi(1)
-
-    def __init__(self, fourier_nn, stdout: Queue = None, num_channels: int = 20):
-        self.stdout = stdout
-        self.pool = None
-        self.live_synth: Process = None
-        self.notes_ready = False
-        self.fourier_nn: FourierNN = fourier_nn
-        self.fs = 44100  # Sample rate
-        self.num_channels = num_channels
-        self.notes: dict = {}
-        self.effects: list[Callable] = [
-            # apply_reverb,
-            # apply_echo,
-            # apply_chorus,
-            # apply_distortion,
-        ]
-        # pygame.init()
-        mixer.init(frequency=44100, size=-16,
-                   channels=2, buffer=1024)
-        mixer.set_num_channels(self.num_channels)
-
-        self.running_channels: dict[str, tuple[int, mixer.Channel]] = {}
-        self.free_channel_ids = list(range(
-            self.num_channels))
-        # self.generate_sounds()
-
-    def generate_sound_wrapper(self, x, offset):
-        midi_note, data = self.fourier_nn.synthesize_tuple(x)
-        return midi_note+offset, data
-
-    def generate_sounds(self):
-        self.notes_ready = False
-
-        timestep = 44100
-
-        t = np.arange(0, 1, step=1 / timestep)
-
-        data = self.fourier_nn.predict((2 * np.pi*t)-np.pi)
-
-        peak_frequency = utils.calculate_peak_frequency(data)
-
-        print("peak_frequency", peak_frequency)
-
-        midi_offset = midi.frequency_to_midi(peak_frequency) \
-            - self.MIDI_NOTE_OF_FREQ_ONE
-        print("Midi Note offset:", midi_offset)
-        self.pool = multiprocessing.Pool(processes=os.cpu_count())
-        atexit.register(self.pool.terminate)
-        atexit.register(self.pool.join)
-        result_async = self.pool.starmap_async(self.generate_sound_wrapper,
-                                               ((x-midi_offset, midi_offset) for x in range(128)))
-        utils.run_after_ms(1000, self.monitor_note_generation, result_async)
-
-    def monitor_note_generation(self, result_async):
-        try:
-            self.note_list = result_async.get(0.1)
-        except multiprocessing.TimeoutError:
-            utils.run_after_ms(
-                1000, self.monitor_note_generation, result_async)
-        else:
-            print("sounds Generated")
-
-            self.notes_ready = True
-            self.play_init_sound()
-            self.pool.close()
-            self.pool.join()
-            atexit.unregister(self.pool.terminate)
-            atexit.unregister(self.pool.join)
-
-    def play_init_sound(self):
-        f1 = 440  # Frequency of the "duuu" sound (in Hz)
-        f2 = 880  # Frequency of the "dib" sound (in Hz)
-        t1 = 0.8  # Duration of the "duuu" sound (in seconds)
-        t2 = 0.2  # Duration of the "dib" sound (in seconds)
-        t = np.arange(int(t1 * self.fs)) / self.fs
-        sound1 = 0.5 * np.sin(2 * np.pi * f1 * t)
-
-        # Generate the "dib" sound
-        t = np.arange(int(t2 * self.fs)) / self.fs
-        sound2 = 0.5 * np.sin(2 * np.pi * f2 * t)
-
-        # Concatenate the two sounds
-        audio = np.concatenate([sound1, sound2])
-        output = np.array(
-            audio * 32767 / np.max(np.abs(audio)) / 2).astype(np.int16)
-        stereo_sine_wave = np.repeat(output.reshape(-1, 1), 2, axis=1)
-        sound = sndarray.make_sound(stereo_sine_wave)
-
-        channel = mixer.Channel(0)
-        channel.play(sound)
-        while (channel.get_busy()):
-            pass
-        print("Ready")
-
-    def apply_effects(self, sound):
-        for effect in self.effects:
-            print(effect.__name__)
-            sound[:] = effect(sound)
-        sound = sound-np.mean(sound)
-        sound[:] = rescale_audio(sound)
-        return sound
-
-    def live_synth_loop(self):
-        midi.init()
-        mixer.init(frequency=44100, size=-16,
-                   channels=2, buffer=1024)
-        mixer.set_num_channels(self.num_channels)
-        for note, sound in self.note_list:
-            # print(sound.shape)
-            stereo = np.repeat(rescale_audio(sound).reshape(-1, 1), 2, axis=1)
-            stereo = np.array(stereo, dtype=np.int16)
-            # np.savetxt(f"tmp/numpy/sound_note_{note}.numpy", stereo)
-            stereo_sound = pygame.sndarray.make_sound(stereo)
-            self.notes[note] = stereo_sound
-
-        input_id = midi.get_default_input_id()
-        if input_id == -1:
-            print("No MIDI input device found.")
-            return
-
-        midi_input = midi.Input(input_id)
-        print("Live Synth is running")
-        while True:
-            if midi_input.poll():
-                midi_events = midi_input.read(10)
-                for midi_event, timestamp in midi_events:
-                    if midi_event[0] == 144:
-                        print("Note on", midi_event[1])
-                        try:
-                            id = self.free_channel_ids.pop()
-                            channel = mixer.Channel(id)
-                            channel.set_volume(1)
-                            channel.play(
-                                self.notes[midi_event[1]], fade_ms=10, loops=-1)
-                            self.running_channels[midi_event[1]] = (
-                                id, channel,)
-                        except IndexError:
-                            print("to many sounds playing")
-                    elif midi_event[0] == 128:
-                        print("Note off", midi_event[1])
-                        self.free_channel_ids.append(
-                            self.running_channels[midi_event[1]][0])
-                        self.running_channels[midi_event[1]][1].stop()
-
-    def pending_for_live_synth(self):
-        if not self.notes_ready:
-            # print("pending")
-            utils.run_after_ms(500, self.pending_for_live_synth)
-        else:
-            self.run_live_synth()
-
-    def run_live_synth(self):
-        if not self.notes_ready:
-            print("\033[31;1;4mNOT READY YET\033[0m")
-            self.generate_sounds()
-            utils.run_after_ms(500, self.pending_for_live_synth)
-            return
-        if not self.live_synth:
-            print("spawning live synth")
-            self.live_synth = Process(target=self.live_synth_loop)
-            self.live_synth.start()
-        else:
-            print("killing live synth")
-            self.live_synth.terminate()
-            self.live_synth.join()
-            self.live_synth = None
-            print("live synth killed")
-        atexit.register(utils.DIE, self.live_synth, 0, 0)
-
-    def play_sound(self, midi_note):
-        id = self.free_channel_ids.pop()
-        channel = mixer.Channel(id)
-        channel.play(self.notes[midi_note], 200)
-
-    def __getstate__(self) -> object:
-        pool = self.pool
-        live_synth = self.live_synth
-        del self.pool
-        del self.live_synth
-        Synth_dict = self.__dict__.copy()
-        self.pool = pool
-        self.live_synth = live_synth
-        return Synth_dict
-
-    def __setstate__(self, state):
-        # Load the model from a file after deserialization
-        self.__dict__.update(state)
-        if self.stdout is not None:
-            if current_process().name != 'MainProcess':
-                sys.stdout = utils.QueueSTD_OUT(queue=self.stdout)
-
-
-def get_raw_audio(sound):
-    return pygame.sndarray.array(sound)
-
-
 def apply_reverb(audio, decay=0.5, delay=0.02, fs=44100):
     delay_samples = int(delay * fs)
     impulse_response = np.zeros((delay_samples, 2))
@@ -280,9 +72,8 @@ def apply_reverb(audio, decay=0.5, delay=0.02, fs=44100):
     reverb_audio = np.zeros_like(audio)
     for channel in range(audio.shape[1]):
         reverb_audio[:, channel] = scipy.signal.fftconvolve(
-            audio[:, channel], impulse_response[:, channel])[:len(audio)]
+                audio[:, channel], impulse_response[:, channel])[:len(audio)]
     return reverb_audio
-
 
 def apply_echo(audio, delay=0.2, decay=0.5, fs=44100):
     delay_samples = int(delay * fs)
@@ -291,7 +82,6 @@ def apply_echo(audio, delay=0.2, decay=0.5, fs=44100):
     echo_audio[delay_samples:] += decay * audio
     echo_audio = echo_audio[:len(audio)]
     return echo_audio
-
 
 def apply_chorus(audio, rate=1.5, depth=0.02, fs=44100):
     t = np.arange(len(audio)) / fs
@@ -306,32 +96,11 @@ def apply_chorus(audio, rate=1.5, depth=0.02, fs=44100):
                 chorus_audio[i, channel] = audio[i, channel]
     return chorus_audio
 
-
 def apply_distortion(audio, gain=2.0, threshold=0.5):
     distorted_audio = gain * audio
     distorted_audio[distorted_audio > threshold] = threshold
     distorted_audio[distorted_audio < -threshold] = -threshold
     return distorted_audio
-
-
-def get_on_notes(notes):
-    return {note: note_data for note, note_data in notes.items() if note_data.on}
-
-
-def get_off_notes(notes):
-    return {note: note_data for note, note_data in notes.items() if not note_data.on}
-
-
-def piano_id():
-    desired_name = "Digital Piano"
-    device_id = None
-
-    for i in range(pygame.midi.get_count()):
-        info = pygame.midi.get_device_info(i)
-        if info[1].decode() == desired_name:
-            device_id = i
-            break
-    return device_id
 
 
 class ADSR:
@@ -345,7 +114,7 @@ class ADSR:
         self.ads_envelope = np.concatenate((
             np.linspace(0, 1, self.attack_samples),
             np.linspace(1, self.sustain_level, self.decay_samples)
-        ))
+            ))
         self.r_envelope = np.linspace(self.sustain_level, 0, self.release_samples)
 
         self._r_counter = [0 for _ in range(128)]
@@ -362,24 +131,24 @@ class ADSR:
         start = self._ads_counter[note_num]
         end = start + frame
         self._ads_counter[note_num] += frame
-        
+
         if start > self.ad_samples:
             return np.ones(frame) * self.sustain_level
-        
+
         envelope = np.zeros(frame)
         if end > self.ad_samples:
             envelope[:self.ad_samples - start] = self.ads_envelope[start:self.ad_samples]
-            envelope[self.ad_samples:] = np.zeros(end - self.ad_samples)
+            envelope[self.ad_samples-start:] = np.ones(end - self.ad_samples)*self.sustain_level
         else:
             envelope[:] = self.ads_envelope[start:end]
-        
+
         return envelope
 
     def get_r_envelope(self, note_num, frame):
         start = self._r_counter[note_num]
         end = start + frame
         self._r_counter[note_num] += frame
-        
+
         if start > self.release_samples:
             return np.zeros(frame)
 
@@ -391,7 +160,7 @@ class ADSR:
             envelope[self.release_samples - start:] = 0  # After release, the envelope is 0
         return envelope
 
-        
+
 class Synth2():
     def __init__(self, fourier_nn, stdout: Queue = None, port_name=None):
         self.stdout = stdout
@@ -402,13 +171,19 @@ class Synth2():
         self.max_parralel_notes = 3
         self.current_frame = 0
         t = np.array([
-            midi.midi_to_frequency(f) *
+            utils.midi_to_freq(f) *
             np.linspace(0, 2*np.pi, self.fs)
             for f in range(128)
-        ])
+            ])
         self.t_buffer = torch.tensor(t, dtype=torch.float32)
         print(self.t_buffer.shape)
         self.port_name = port_name
+        self.adsr_envelope=ADSR(
+                0.1,
+                0.1,
+                0.7,
+                0.2,
+                self.fs)
 
     def set_port_name(self, port_name):
         self.port_name = port_name
@@ -442,7 +217,7 @@ class Synth2():
         # Concatenate the sounds to form "duuudldiiidub"
         audio = np.concatenate([sound1, sound2, sound3, sound4])
         output = np.array(
-            audio * 32767 / np.max(np.abs(audio)) / 2).astype(np.int16)
+                audio * 32767 / np.max(np.abs(audio)) / 2).astype(np.int16)
         sd.play(output, blocking=True)
         print("Ready")
 
@@ -480,26 +255,37 @@ class Synth2():
                     if midi_event.type == 'note_on':  # Note on
                         print("Note on",
                               midi_event.note,
-                              midi.midi_to_frequency(midi_event.note))
+                              utils.midi_to_freq(midi_event.note))
 
-                        notes[midi_event.note] = midi_event.velocity
+                        notes[midi_event.note] = [True, midi_event.velocity]
+                        self.adsr_envelope.reset_note(midi_event.note)
                     elif midi_event.type == 'note_off':  # Note off
                         print("Note off",
                               midi_event.note,
-                              midi.midi_to_frequency(midi_event.note))
+                              utils.midi_to_freq(midi_event.note))
                         if midi_event.note in notes:
-                            del notes[midi_event.note]
+                            #del notes[midi_event.note]
+                            notes[midi_event.note][0]=False 
 
                 if len(notes) > 0:
                     y = np.zeros((len(notes), available_buffer),
                                  dtype=np.float32)
-                    for i, (note, amplitude) in enumerate(notes.items()):
+                    to_delete=[]
+                    for i, (note, data) in enumerate(notes.items()):
                         with torch.no_grad():
                             x = utils.wrap_concat(
-                                self.t_buffer[note], cycle_frame, cycle_frame + available_buffer).to(self.fourier_nn.device)
-
+                                    self.t_buffer[note], cycle_frame, cycle_frame + available_buffer).to(self.fourier_nn.device)
+                            if data[0]:
+                                envelope=self.adsr_envelope.get_ads_envelope(note, available_buffer)
+                            else:
+                                envelope=self.adsr_envelope.get_r_envelope(note, available_buffer)
+                                if self.adsr_envelope.has_note_ended(note):
+                                    to_delete.append(note)
                             y[i, :] = model(
-                                x.unsqueeze(1)).cpu().numpy().astype(np.float32)  # * (amplitude/127)
+                                    x.unsqueeze(1)).cpu().numpy().astype(np.float32) * envelope # * (amplitude/127)
+
+                    for note in to_delete:
+                        del notes[note]
 
                     audio_data = sum_signals(y)
                     # print(np.max(np.abs(audio_data)))
@@ -597,7 +383,7 @@ def sum_signals_torch(signals):
 
     # Normalize each signal
     normalized_signals = torch.stack(
-        [sum_signals_torch(signal) for signal in signals])
+            [sum_signals_torch(signal) for signal in signals])
 
     # Sum the signals along dimension 0
     summed_signal = torch.sum(normalized_signals, dim=0)
