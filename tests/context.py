7aac8e66a239aa7a25ae2f2411a76cf6fe3b93f0
thetechnicker <https.LocalHorst@gmx.de>
2025-02-25 16:07:22 +0100

    test contex fixed

--- tests/context.py
+++ tests/context.py
@@ -1,10 +1,7 @@
 import os
 import sys
 
-# autopep8: off
-path=os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, path)
 
-import src
-import main
-# autopep8: on
+import pythonaisynth
