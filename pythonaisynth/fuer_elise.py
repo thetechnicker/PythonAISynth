363b9402754abe02cb6225e20c3e9b5aed2a2ca4
thetechnicker <https.LocalHorst@gmx.de>
2025-03-26 14:05:57 +0100

    more usable fuer eliese

--- pythonaisynth/fuer_elise.py
+++ pythonaisynth/fuer_elise.py
@@ -1,83 +1,97 @@
+import multiprocessing
 import mido
 import time
 import os
+from multiprocessing.synchronize import Event
 
-# Define the MIDI output port
-if os.name == "nt":
-    output = mido.open_output("LoopBe Internal MIDI 1")
-else:
+
+def play_fur_elise(stop_event: Event, port_name=None):
+    if port_name:
+        output = mido.open_output(port_name)
+    else:
+        # Define the MIDI output port
+        if os.name == "nt":
+            output = mido.open_output("LoopBe Internal MIDI 1")
+        else:
+            try:
+                output = mido.open_output("Midi Through Port-0")
+            except:
+                print("no midi loopback found")
+                return
+
+    # Define the notes for "Für Elise"
+    fur_elise = [
+        ("E5", 0.5),
+        ("D#5", 0.5),
+        ("E5", 0.5),
+        ("D#5", 0.5),
+        ("E5", 0.5),
+        ("B4", 0.5),
+        ("D5", 0.5),
+        ("C5", 0.5),
+        ("A4", 1.5),
+        ("C4", 0.5),
+        ("E4", 0.5),
+        ("A4", 0.5),
+        ("B4", 1.5),
+        ("E4", 0.5),
+        ("G#4", 0.5),
+        ("B4", 0.5),
+        ("C5", 1.5),
+        ("E4", 0.5),
+        ("C4", 0.5),
+        ("E4", 0.5),
+        ("A4", 0.5),
+        ("B4", 1.5),
+        ("E4", 0.5),
+        ("G#4", 0.5),
+        ("B4", 0.5),
+        ("C5", 1.5),
+    ]
+
+    # Define a function to convert note names to MIDI note numbers
+    def note_to_midi(note):
+        note_map = {
+            "C": 0,
+            "C#": 1,
+            "D": 2,
+            "D#": 3,
+            "E": 4,
+            "F": 5,
+            "F#": 6,
+            "G": 7,
+            "G#": 8,
+            "A": 9,
+            "A#": 10,
+            "B": 11,
+        }
+        octave = int(note[-1])
+        key = note[:-1]
+        return 12 * (octave + 1) + note_map[key]
+
     try:
-        output = mido.open_output("Midi Through Port-0")
+        # Stream the notes over MIDI in a loop
+        last_note = None
+        while not stop_event.is_set():
+            for note, duration in fur_elise:
+                if stop_event.is_set():
+                    break
+                midi_note = note_to_midi(note)
+                last_note = midi_note
+                output.send(mido.Message("note_on", note=midi_note, velocity=64))
+                time.sleep(duration / 2)
+                output.send(mido.Message("note_off", note=midi_note, velocity=64))
     except:
-        print("no midi loopback found")
-        exit()
+        output.panic()
+    finally:
+        if last_note:
+            output.send(mido.Message("note_off", note=last_note, velocity=64))
+        output.close()
 
 
-# Define the notes for "Für Elise"
-fur_elise = [
-    ("E5", 0.5),
-    ("D#5", 0.5),
-    ("E5", 0.5),
-    ("D#5", 0.5),
-    ("E5", 0.5),
-    ("B4", 0.5),
-    ("D5", 0.5),
-    ("C5", 0.5),
-    ("A4", 1.5),
-    ("C4", 0.5),
-    ("E4", 0.5),
-    ("A4", 0.5),
-    ("B4", 1.5),
-    ("E4", 0.5),
-    ("G#4", 0.5),
-    ("B4", 0.5),
-    ("C5", 1.5),
-    ("E4", 0.5),
-    ("C4", 0.5),
-    ("E4", 0.5),
-    ("A4", 0.5),
-    ("B4", 1.5),
-    ("E4", 0.5),
-    ("G#4", 0.5),
-    ("B4", 0.5),
-    ("C5", 1.5),
-]
-
-# Define a function to convert note names to MIDI note numbers
-
-
-def note_to_midi(note):
-    note_map = {
-        "C": 0,
-        "C#": 1,
-        "D": 2,
-        "D#": 3,
-        "E": 4,
-        "F": 5,
-        "F#": 6,
-        "G": 7,
-        "G#": 8,
-        "A": 9,
-        "A#": 10,
-        "B": 11,
-    }
-    octave = int(note[-1])
-    key = note[:-1]
-    return 12 * (octave + 1) + note_map[key]
-
-
-try:
-    # Stream the notes over MIDI in a loop
-    last_note = None
-    while True:
-        for note, duration in fur_elise:
-            midi_note = note_to_midi(note)
-            last_note = midi_note
-            output.send(mido.Message("note_on", note=midi_note, velocity=64))
-            time.sleep(duration / 2)
-            output.send(mido.Message("note_off", note=midi_note, velocity=64))
-except:
-    output.panic()
-output.send(mido.Message("note_off", note=last_note, velocity=64))
-# Close the MIDI output port (this line will never be reached due to the infinite loop)
-# output.close()
+if __name__ == "__main__":
+    event = multiprocessing.Event()
+    try:
+        play_fur_elise(event)
+    except KeyboardInterrupt:
+        event.set()
